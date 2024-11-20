9a4bbec3845275efe1c9b6c1f4c609c3624c07e0
heogeonho <geonh1016@gmail.com>
2024-11-20 10:14:45 +0900

    init: 프로젝트 생성
    spring-boot-starter-data-jpa, spring-boot-starter-web, lombok, spring-boot-devtools, mysql-connector-j 추가

---
+++ src/main/java/com/example/paymentgatewaypractice/PaymentGatewayPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.paymentgatewaypractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentGatewayPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentGatewayPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/paymentgatewaypractice/PaymentGatewayPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paymentgatewaypractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentGatewayPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
