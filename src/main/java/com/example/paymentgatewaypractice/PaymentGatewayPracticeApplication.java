8b50ff5c3f6091d88c5e1f52fa5042ef82a5b0f7
건호 <111233054+heogeonho@users.noreply.github.com>
2025-01-19 22:52:44 +0900

    PaymentGatewayPracticeApplication.java 업데이트

--- src/main/java/com/example/paymentgatewaypractice/PaymentGatewayPracticeApplication.java
+++ src/main/java/com/example/paymentgatewaypractice/PaymentGatewayPracticeApplication.java
@@ -3,6 +3,7 @@ package com.example.paymentgatewaypractice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PaymentGatewayPracticeApplication {
 
